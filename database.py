423b23e8cca11443cd0008a38380657551f532f8
yasin-arafat-05 <yasinarafat.e2021@gmail.com>
2024-03-06 00:48:35 +0600

    fastapi_restapi_database

--- database.py
+++ database.py
@@ -1,7 +1,19 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+from dotenv import dotenv_values
 
+config_credential = dotenv_values(".env")
 
+POSTGRES_DATABASE_URL = f"{config_credential["DB_CONNECTION"]}://{config_credential["DB_USERNAME"]}:{config_credential['DB_PASSWORD']}@{config_credential['DB_HOST']}:{config_credential['DB_PORT']}/{config_credential['DB_DATABASE']}"
 
-POSTGRES_DATABASE_URL = "postgresql://user:password@postgresserver/db"
+engine = create_engine(
+    POSTGRES_DATABASE_URL
+)
+
+sessionLocal = sessionmaker(
+    autoflush=False,autocommit = False,bind=engine
+)
+
 Base = declarative_base()
+
